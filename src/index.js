07e4f6ac1cdf660ab0f6770958068f71a8cc8275
kwYoohae <gocksdl7979@gmail.com>
2022-09-27 12:28:23 +0900

    [추가] map 구현 및  mark 예제 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Map from "./component/Map";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <div>
+          <Map>
+
+          </Map>
+      </div>
   </React.StrictMode>
 );
 
